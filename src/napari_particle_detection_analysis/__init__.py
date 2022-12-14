ad5cd6dcc8dee4539a45dd1acc3d4f7be780322a
bpavie <benjamin.pavie@gmail.com>
2022-12-14 12:03:58 +0100

    first version dividing the pipeline in 3 stages (cell segmentation. particle segmentation, particle analysis)

--- src/napari_particle_detection_analysis/__init__.py
+++ src/napari_particle_detection_analysis/__init__.py
@@ -1,10 +1,10 @@
 __version__ = "0.0.1"
-from ._widget import ExampleQWidget, example_magic_widget
+from ._widget import SegmentationWidget, ParticleSegmentationWidget, ParticleCellAnalysis
 from ._writer import write_multiple, write_single_image
 
 __all__ = (
     "write_single_image",
     "write_multiple",
-    "ExampleQWidget",
-    "example_magic_widget",
-)
+    "SegmentationWidget",
+    "ParticleSegmentationWidget",
+    "ParticleCellAnalysis")
